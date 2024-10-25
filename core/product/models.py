bf4831b2ba2c7bb78413df7fa0038ad8204bc796
balasubramani-ide <balasubramani.ide@gmail.com>
2024-10-25 16:59:19 +0530

    "Renamed several models and fields in core/base and core/product, updated related imports and references; removed comment in admin.py; updated db.sqlite3"

--- core/product/models.py
+++ core/product/models.py
@@ -3,29 +3,29 @@ from base.models import BaseModel
 from django.utils.text import slugify
 
 
-class Manufacturer(BaseModel):
+class ProductManufacturer(BaseModel):
     name = models.CharField(max_length= 256, null= False, blank= False, unique= True)
     description = models.TextField(max_length= 1024, null= True, blank= True, default= " ")
     logo = models.ImageField(upload_to="product/manufacturer_logo", null= True, blank= True)
     slug = models.SlugField(unique=True, null = True, blank = True)
     def save(self, *args , **kwargs):
         self.slug = slugify(self.name)
-        super(Manufacturer,self).save(*args, **kwargs)
+        super(ProductManufacturer,self).save(*args, **kwargs)
     def __str__(self):
         return self.name
 
-class Brand(BaseModel):
-    manufacturer = models.ForeignKey(Manufacturer , on_delete= models.CASCADE, related_name="brand")
+class ProductBrand(BaseModel):
+    manufacturer = models.ForeignKey(ProductManufacturer , on_delete= models.CASCADE, related_name="brand")
     name = models.CharField(max_length=256, null = False, blank= False, unique= True)
     logo = models.ImageField(upload_to="product/brand_logo", null= True, blank= True)
     slug = models.SlugField(unique=True, null = True, blank = True)
     def save(self, *args , **kwargs):
         self.slug = slugify(self.name)
-        super(Brand,self).save(*args, **kwargs)
+        super(ProductBrand,self).save(*args, **kwargs)
     def __str__(self):
         return self.name
     
-class Category(BaseModel):
+class ProductCategory(BaseModel):
     slug = models.SlugField(unique=True, null = True, blank = True)
     name = models.CharField(max_length= 64, null= False, blank= False, unique= True)
     logo = models.ImageField(upload_to="product/category_logo", null= True, blank= True)
@@ -33,41 +33,41 @@ class Category(BaseModel):
     parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subcategories')
     def save(self, *args , **kwargs):
         self.slug = slugify(self.name)
-        super(Category,self).save(*args, **kwargs)
+        super(ProductCategory,self).save(*args, **kwargs)
     def __str__(self):
         return self.name
     
 
-class VariantType(BaseModel):
+class ProductVariantType(BaseModel):
     name = models.CharField(max_length=64, unique=True)
     slug = models.SlugField(unique=True, null = True, blank = True)
     def save(self, *args , **kwargs):
         self.slug = slugify(self.name)
-        super(VariantType,self).save(*args, **kwargs)
+        super(ProductVariantType,self).save(*args, **kwargs)
     def __str__(self):
         return self.name
 
 
-class Variant(BaseModel):
-    variant_type = models.ForeignKey(VariantType, on_delete=models.CASCADE, related_name='variant')
+class ProductVariantValue(BaseModel):
+    variant_type = models.ForeignKey(ProductVariantType, on_delete=models.CASCADE, related_name='variant')
     value = models.CharField(max_length=64)
     slug = models.SlugField(unique=True, null = True, blank = True)
     def save(self, *args , **kwargs):
         self.slug = slugify(self.name)
-        super(Variant,self).save(*args, **kwargs)
+        super(ProductVariantValue,self).save(*args, **kwargs)
     def __str__(self):
         return f"{self.variant_type.name}: {self.value}"
 
 
 class Product(BaseModel):
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='product')
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='product')
-    categories = models.ManyToManyField(Category, related_name='product')
+    manufacturer = models.ForeignKey(ProductManufacturer, on_delete=models.CASCADE, related_name='product')
+    brand = models.ForeignKey(ProductBrand, on_delete=models.CASCADE, related_name='product')
+    categories = models.ManyToManyField(ProductCategory, related_name='product')
     name = models.CharField(max_length=512)
     description = models.TextField()
     retail_price = models.DecimalField(max_digits=10, decimal_places=2)
     selling_price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField(default=0)
     slug = models.SlugField(unique=True, null = True, blank = True)
     def save(self, *args , **kwargs):
         self.slug = slugify(self.name)
@@ -78,7 +78,7 @@ class Product(BaseModel):
 
 class ProductVariant(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_variant')
-    variant = models.ForeignKey(Variant, on_delete=models.CASCADE, related_name='product_variant')
+    variant = models.ForeignKey(ProductVariantValue, on_delete=models.CASCADE, related_name='product_variant')
     price_override = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     stock = models.PositiveIntegerField(default=0)
     slug = models.SlugField(unique=True, null = True, blank = True)
@@ -90,7 +90,7 @@ class ProductVariant(BaseModel):
     def __str__(self):
         return f"{self.product.name} - {self.variant.value}"
 
-class Product_Images(BaseModel):
+class ProductImages(BaseModel):
     product = models.ForeignKey(Product, on_delete = models.CASCADE, related_name = "images")
     image = models.ImageField(upload_to="product/images")
     def __str__(self):
