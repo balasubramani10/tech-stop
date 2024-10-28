8afe9ab850a2a3b8a839944a8deef42e8261ea1b
balasubramani-ide <balasubramani.ide@gmail.com>
2024-10-28 11:40:56 +0530

    "Added A Custom Admin Panel .

--- core/core/settings.py
+++ core/core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin', #custom Admin Panel (django-jazzmin)
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
